c2e3d18365d5f24369bf4a16bd14754d5792caf0
aliciagarciasierra <134939956+aliciagarciasierra@users.noreply.github.com>
2025-01-22 09:52:29 +0000

    Add files via upload

---
+++ 00_MASTER_WLS.R
@@ -0,0 +1,101 @@
+
+
+##############################################################
+######## LIBERAL AND RADICAL EOP ##########################
+######## USING WISCONSIN LONGITUDINAL STUDY #################
+##############################################################
+
+# Authors: Michael Grätz, Alicia García-Sierra & Sonia Petrini
+
+# Data: WLS
+
+# Date of preparation of this script: Sept 2025
+
+#######################################################
+#########   PREPARE THE ENVIRONMENT ################
+######################################################
+
+# CLEAN
+
+rm(list=ls()) 
+
+# LOAD PACKAGES
+
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(haven)
+  library(stringr)
+  library(tidyr)
+  library(labelled)
+  library(readxl)
+  library(lme4)
+  library(fixest)
+  library(writexl)
+  library(dplyr)
+  library(boot)
+  library(Matrix) # note that an updated version of R might be required for this package to work correctly in our analyses
+  library(openxlsx)
+  library(ggplot2)
+  library(pilot)
+  library(dplyr)
+  library(ggpubr)
+  library(missMDA)
+  library(parallel)
+})
+
+`%!in%` <- function(x, y) !(x %in% y)
+# SET WD 
+
+setwd("/Users/agarcias/Library/CloudStorage/OneDrive-UniversitédeLausanne/EQUALOPP/PROJECT/WLS/data")
+
+
+# OPEN RAW DATA (first time)
+
+#data <- read_dta("wls_bl_14_03.dta") # main dataset
+#pgi_cog<- read_dta("Lee_idpub_shuffled.dta") # PGIs cognitive
+#pgi_noncog <- read_dta("Turley_idpub_shuffled.dta") # PGIs non-cognitive
+
+
+# SAVE IN RDA (for faster read following times)
+
+#saveRDS(data, file = "data.rds")
+#saveRDS(pgi_cog, file = "pgi_cog.rds")
+#saveRDS(pgi_noncog, file = "pgi_noncog.rds")
+
+
+# GLOBALS 
+INDICES <- c("Sibcorr","IOLIB", "IORAD")
+INDICES.labs <- c("Sibcorr" = "Sibling correlation", "IOLIB" = "Liberal IOP", "IORAD" = "Radical IOP")
+
+# outcomes
+OUTCOMES <- c("education", "occupation","income", "wealth","health_pc")
+OUTCOMES.labs <- c("education" = "Education", "occupation" = "Occupation", 
+  "income_ind" = "Income Ind", "income" = "Income", 
+  "wealth" = "Wealth", "wealth_built" = "Built wealth", "health_self" = "Health Self-Rep", 
+  "health_illness" = "Health N Illnesses", "health_pc" = "Health")
+
+# ascribed
+ASCRIBED <- c("sex", "birth_year", "mother_age_birth", "father_age_birth", "birth_order")
+
+# ability
+ABILITY_DEFS <- c("polygenic indices", "observed ability")
+
+# - observed
+OBSERVED_NON_COG <- c("extraversion", "openness", "neuroticism", "conscientiousness", "agreeableness")
+OBSERVED_COG     <- "centile_rank_IQ"  # or "IQ"
+
+# - pgi
+PGI_COG     <- c("pgi_education", "pgi_cognitive", "pgi_math_exam", "pgi_math_ability")
+PGI_NON_COG <- c("pgi_depression", "pgi_well_being", "pgi_neuroticism")
+PGIs <- c(PGI_COG, PGI_NON_COG)
+PC_COG     <- paste0("pc", 1:10, "cog")
+#PC_NON_COG <- paste0("pc", 1:10, "noncog")
+
+
+# settings
+n_boot <- 100
+
+
+
+
+
---
+++ 04_DESCRIPTIVES.R
@@ -0,0 +1,227 @@
+
+###################################################################################
+###################### DESCRIPTIVES  ###########################################
+###################################################################################
+
+source("00_MASTER_WLS.R")
+siblings <- readRDS("siblings.rds")
+
+siblings <- siblings %>%
+  mutate_if(is.numeric, scale)
+
+
+
+####################################################################
+################## TABLE DESCRIPTIVES ##############################
+####################################################################
+
+# Extract variables from the globals
+variables_of_interest <- c(
+  get("OUTCOMES"),
+  get("ASCRIBED"),
+  get("OBSERVED_NON_COG"),
+  get("OBSERVED_COG"),
+  get("PGI_COG"),
+  get("PGI_NON_COG")
+)
+
+# Check the resulting list
+print(variables_of_interest)
+
+# Filter the dataset to include only the variables of interest
+descriptive_stats <- siblings %>%
+  select(all_of(variables_of_interest)) %>%
+  summarise(across(everything(), list(
+    mean = ~ mean(.x, na.rm = TRUE),
+    min = ~ min(.x, na.rm = TRUE),
+    max = ~ max(.x, na.rm = TRUE),
+    sd = ~ sd(.x, na.rm = TRUE)
+  )))
+
+# Reshape for better readability
+descriptive_stats_long <- descriptive_stats %>%
+  pivot_longer(
+    cols = everything(),
+    names_to = c("Variable", "Statistic"),
+    names_pattern = "^(.*)_(mean|min|max|sd)$",  # Match suffixes without splitting variable names
+    values_to = "Value"
+  ) %>%
+  pivot_wider(
+    names_from = Statistic,
+    values_from = Value
+  )
+# View the result
+print(descriptive_stats_long)
+
+write_xlsx(descriptive_stats_long, path = "descriptives.xlsx")
+
+####################################################################
+################## FIGURE 1: HEXBIN CHART ##############################
+####################################################################
+
+siblings <- readRDS("siblings.rds")
+
+# Reshape to wide at the family level
+wide<-reshape(as.data.frame(siblings),direction="wide", idvar="familyID", timevar="withinID")
+
+
+##############
+#### PGIs ####
+##############
+
+# Loop to generate the plots
+sapply(PGIs, function(pgi){
+  # Dynamically create the column names for sibling 1 and sibling 2
+  x_var <- paste0(pgi, ".1")
+  y_var <- paste0(pgi, ".2")
+  
+  # Create the hexbin plot for each PGI variable
+  hexbin <- ggplot(wide, aes_string(x=x_var, y=y_var)) +
+    geom_hex(bins = 80) +
+    geom_abline(color="darkgoldenrod1", linetype="dashed", size=0.3) +
+    theme_bw() +
+    scale_x_continuous(breaks=c(-0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4)) +
+    scale_y_continuous(breaks=c(-0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4)) +
+    scale_fill_continuous(type = "viridis") +
+    xlab("Sibling 1") +
+    ylab("Sibling 2") +
+    theme(axis.title.y = element_text(size = 11, angle = 90)) +
+    theme(axis.title.x = element_text(size = 11, angle = 00)) +
+    theme(legend.title = element_text(color = "black", size = 8),
+          legend.text = element_text(color = "black", size = 7)) +
+    ggtitle(paste("Hexbin chart: differences in the", gsub("pgi_", "", pgi), "PGI between siblings")) +
+    theme(plot.title = element_text(size=12, face="bold"))
+  
+  # Display the plot with the correct fill label
+  print(hexbin + labs(fill = "Count"))
+})
+
+
+
+##############
+##### PCs ####
+##############
+
+# Loop to generate the plots
+sapply(PC_COG, function(pc){
+  # Dynamically create the column names for sibling 1 and sibling 2
+  x_var <- paste0(pc, ".1")
+  y_var <- paste0(pc, ".2")
+  
+  # Create the hexbin plot for each PGI variable
+  hexbin <- ggplot(wide, aes_string(x=x_var, y=y_var)) +
+    geom_hex(bins = 80) +
+    geom_abline(color="darkgoldenrod1", linetype="dashed", linewidth=0.3) +
+    theme_bw() +
+    scale_x_continuous(breaks=c(-0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4)) +
+    scale_y_continuous(breaks=c(-0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4)) +
+    scale_fill_continuous(type = "viridis") +
+    xlab("Sibling 1") +
+    ylab("Sibling 2") +
+    theme(axis.title.y = element_text(size = 11, angle = 90)) +
+    theme(axis.title.x = element_text(size = 11, angle = 00)) +
+    theme(legend.title = element_text(color = "black", size = 8),
+          legend.text = element_text(color = "black", size = 7)) +
+    ggtitle(paste("Hexbin chart: differences in the", gsub("pc_", "", pc), "PC between siblings")) +
+    theme(plot.title = element_text(size=12, face="bold"))
+  
+  # Display the plot with the correct fill label
+  print(hexbin + labs(fill = "Count"))
+})
+
+
+
+
+####################################################################
+################## FIGURE 2: PGI vs OBSERVED #######################
+####################################################################
+
+
+datas <- lapply(ABILITY_DEFS, function(ability) {
+  name <- switch(ability, "polygenic indices"="", "observed ability"=paste0("_",OBSERVED_COG))
+  data <- read_excel(paste0("full_results",name,".xlsx"), sheet = "For plotting")
+  data %>% mutate(Ability = ability)
+})
+data_graph <- do.call(rbind, datas)
+
+# Only outcomes of interest
+data_graph <- data_graph %>% filter(Outcome %in% OUTCOMES)
+
+# Custom order 
+data_graph <- data_graph %>% mutate(
+  Index   = factor(Index,   levels = INDICES),
+  Outcome = factor(Outcome, levels = OUTCOMES),
+  Ability = factor(Ability, levels = ABILITY_DEFS)
+)
+
+n <- nrow(siblings)
+
+# labels with sample size
+custom_labels <- c("polygenic indices" = paste0("polygenic indices (n=", n, ")"), "observed ability" = paste0("observed ability (n=", n, ")"))
+
+
+ggplot(data_graph, aes(x = Outcome, y = Estimate, fill = Index)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  geom_errorbar(aes(ymin = Lower, ymax = Upper), 
+                position = position_dodge(0.9), width = 0.25, alpha = 0.4) +
+  labs(title = " ", x = " ", y = " ") +
+  geom_text(aes(label = round(Estimate, 2)), position = position_dodge(width = 1), vjust =-1.5 ,hjust=0.3) + 
+  
+  # Add labels
+  scale_x_discrete(labels = OUTCOMES.labs) +
+  scale_fill_discrete(labels = INDICES.labs) +
+  # Theme
+  guides(fill = guide_legend(title = NULL)) +
+  facet_wrap(~ Ability, nrow = 2, labeller = labeller(Ability = custom_labels)) +
+  ylim(-0.1,0.5) +
+  theme_bw()
+
+ggsave(paste0("plots/comparison_rows.pdf"), width = 11, height = 8, dpi = 300)
+   
+
+####################################################################
+###################### SIBLING SIMILARITY ##########################
+####################################################################
+
+# Reshape to wide at the family level
+wide<-reshape(as.data.frame(siblings),direction="wide", idvar="familyID", timevar="withinID")
+
+# compute mean outcome differences
+mean_diff <- lapply(OUTCOMES, function(var) {
+  x_var <- paste0(var, ".1")
+  y_var <- paste0(var, ".2")
+  
+  all_diff <- wide %>% reframe(diff = abs(get(x_var, wide) - get(y_var, wide))) %>% unlist()
+  m <- mean(all_diff) %>% round(2) # 1.851
+  list("outcome" = var, "mean_sib_diff" = m)
+})
+
+# save
+diff_tab <- do.call(rbind.data.frame, mean_diff) %>% cbind("unit" = c("years", "Duncan SEI score", "Dollars", "Dollars", "PC"))
+write_csv(diff_tab, "results/siblings_diff_outcomes.csv")
+
+
+
+# compute mean differences
+
+mean_diff <- lapply(PGIs, function(var) {
+  
+  x_var <- paste0(var, ".1")
+  y_var <- paste0(var, ".2")
+  
+  all_diff <- wide %>% 
+    reframe(diff = abs(get(x_var, wide) - get(y_var, wide))) %>% unlist()
+  m <- mean(all_diff) %>% round(2) # 1.851
+  list("PGI" = var, "mean_sib_diff" = m)
+})
+
+# save
+diff_tab <- do.call(rbind.data.frame, mean_diff) 
+write_csv(diff_tab, "results/siblings_diff_PGIs.csv")
+
+
+
+
+
+
+
